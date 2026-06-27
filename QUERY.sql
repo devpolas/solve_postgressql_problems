8168e8036495c7ab8285026fe5e29f63805ddb94
devpolas <195308698+devpolas@users.noreply.github.com>
2026-06-28 02:07:50 +0600

    fix bug for query 1

--- QUERY.sql
+++ QUERY.sql
@@ -174,7 +174,7 @@ VALUES
 
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 
-SELECT * FROM Matches WHERE match_status = 'Available';
+SELECT * FROM Matches WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 
